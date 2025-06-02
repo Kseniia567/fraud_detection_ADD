beb17825302a043ec4cf0348f8566fad773dc720
Ксения Смирнова <kseniasmirnova@MacBook-Pro-Ksenia.local>
2025-06-02 21:43:10 +0200

    Remove unnecessary

--- src/rabbit_mq/rabbitmq_init.py
+++
@@ -1,8 +0,0 @@
-import pika
-
-def create_channel(queue_name):
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
-    channel =  connection.channel()
-    channel.queue_declare(queue = queue_name)
-    return channel
-
